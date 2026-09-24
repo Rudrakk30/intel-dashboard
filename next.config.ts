0877964955eb521141bb051f22b5f32200f4404c
Rudrakk30 <161931439+Rudrakk30@users.noreply.github.com>
2026-09-24 11:32:59 +0530

    Fix next.config.ts TypeScript error

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    buildActivity: false,
-    appIsrStatus: false,
-  },
   async headers() {
     return [
       {
